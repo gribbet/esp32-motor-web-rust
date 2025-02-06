a8c9a4d563b9e9700aa7e83446b816dd577e79b4
Graham <graham.gibbons@gmail.com>
2025-02-06 14:34:25 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+use esp_backtrace as _;
+use esp_hal::{
+    delay::Delay,
+    gpio::{Level, Output},
+    main,
+};
+use esp_println::println;
+
+#[main]
+fn main() -> ! {
+    let peripherals = esp_hal::init(esp_hal::Config::default());
+
+    println!("Hello world!");
+
+    let mut led = Output::new(peripherals.GPIO7, Level::Low);
+    led.set_high();
+
+    let delay = Delay::new();
+
+    loop {
+        led.toggle();
+        delay.delay_millis(500);
+    }
+}
